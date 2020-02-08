ad22fc32356023794be65857c3cd41215db7f940
steschu77 <steschu77@gmail.com>
2020-02-08 10:17:57 +0100

    Remove box-based collision code

--- src/Collide.cpp
+++ src/Collide.cpp
@@ -12,35 +12,6 @@
 #include "Arbiter.h"
 #include "Body.h"
 
-// Box vertex and edge numbering:
-//
-//        ^ y
-//        |
-//        e1
-//   v2 ------ v1
-//    |        |
-// e2 |        | e4  --> x
-//    |        |
-//   v3 ------ v4
-//        e3
-
-enum Axis
-{
-  FACE_A_X,
-  FACE_A_Y,
-  FACE_B_X,
-  FACE_B_Y
-};
-
-enum EdgeNumbers
-{
-  NO_EDGE = 0,
-  EDGE1,
-  EDGE2,
-  EDGE3,
-  EDGE4
-};
-
 struct ClipVertex
 {
   ClipVertex() { fp.value = 0; }
@@ -179,82 +150,16 @@ int ClipSegmentToLine(ClipVertex vOut[2], ClipVertex vIn[2],
     if (distance0 > 0.0f) {
       vOut[numOut].fp = vIn[0].fp;
       vOut[numOut].fp.e.inEdge1 = clipEdge;
-      vOut[numOut].fp.e.inEdge2 = NO_EDGE;
+      vOut[numOut].fp.e.inEdge2 = 0;
     } else {
       vOut[numOut].fp = vIn[1].fp;
       vOut[numOut].fp.e.outEdge1 = clipEdge;
-      vOut[numOut].fp.e.outEdge2 = NO_EDGE;
+      vOut[numOut].fp.e.outEdge2 = 0;
     }
     ++numOut;
   }
 
   return numOut;
-}
-
-static void ComputeIncidentEdge(ClipVertex c[2], const x3d::vector2& h,
-  const x3d::vector2& pos, const x3d::matrix2x2& Rot, const x3d::vector2& normal)
-{
-  // The normal is from the reference box. Convert it
-  // to the incident boxe's frame and flip sign.
-  x3d::matrix2x2 RotT = Rot.transpose();
-  x3d::vector2 n = -(RotT * normal);
-  x3d::vector2 nAbs = n.abs();
-
-  if (nAbs.u0() > nAbs.u1()) {
-    if (n.u0() > 0.0f) {
-      c[0].v = x3d::vector2(h.u0(), -h.u1());
-      c[0].fp.e.inEdge2 = EDGE3;
-      c[0].fp.e.outEdge2 = EDGE4;
-
-      c[1].v = x3d::vector2(h.u0(), h.u1());
-      c[1].fp.e.inEdge2 = EDGE4;
-      c[1].fp.e.outEdge2 = EDGE1;
-    } else {
-      c[0].v = x3d::vector2(-h.u0(), h.u1());
-      c[0].fp.e.inEdge2 = EDGE1;
-      c[0].fp.e.outEdge2 = EDGE2;
-
-      c[1].v = x3d::vector2(-h.u0(), -h.u1());
-      c[1].fp.e.inEdge2 = EDGE2;
-      c[1].fp.e.outEdge2 = EDGE3;
-    }
-  } else {
-    if (n.u1() > 0.0f) {
-      c[0].v = x3d::vector2(h.u0(), h.u1());
-      c[0].fp.e.inEdge2 = EDGE4;
-      c[0].fp.e.outEdge2 = EDGE1;
-
-      c[1].v = x3d::vector2(-h.u0(), h.u1());
-      c[1].fp.e.inEdge2 = EDGE1;
-      c[1].fp.e.outEdge2 = EDGE2;
-    } else {
-      c[0].v = x3d::vector2(-h.u0(), -h.u1());
-      c[0].fp.e.inEdge2 = EDGE2;
-      c[0].fp.e.outEdge2 = EDGE3;
-
-      c[1].v = x3d::vector2(h.u0(), -h.u1());
-      c[1].fp.e.inEdge2 = EDGE3;
-      c[1].fp.e.outEdge2 = EDGE4;
-    }
-  }
-
-  c[0].v = pos + Rot * c[0].v;
-  c[1].v = pos + Rot * c[1].v;
-}
-
-// ----------------------------------------------------------------------------
-template <typename T, size_t size>
-size_t idxOfMin(const T (&x)[size])
-{
-  size_t idx = 0;
-
-  for (size_t i = 1; i < size; i++) {
-    if (x[i] < x[idx]) {
-      idx = i;
-    }
-  }
-
-  return idx;
 }
 
 // The normal points from A to B
@@ -294,147 +199,33 @@ int Collide(Contact* contacts, Body* bodyA, Body* bodyB)
 
 	ClipVertex incedge1[2];
   int np1 = ClipSegmentToLine(incedge1, incedge, -tangent, sideOffset1, iv1);
+  if (np1 < 2) {
+    return 0;
+  }
 
 	ClipVertex incedge2[2];
   int np2 = ClipSegmentToLine(incedge2, incedge1, tangent, sideOffset2, iv2);
-
-  // Setup
-  x3d::vector2 hA = 0.5f * bodyA->width;
-  x3d::vector2 hB = 0.5f * bodyB->width;
-
-  x3d::vector2 posA = bodyA->position;
-  x3d::vector2 posB = bodyB->position;
-
-  x3d::matrix2x2 RotA(bodyA->rotation);
-  x3d::matrix2x2 RotB(bodyB->rotation);
-
-  x3d::matrix2x2 RotAT = RotA.transpose();
-  x3d::matrix2x2 RotBT = RotB.transpose();
-
-  x3d::vector2 dp = posB - posA;
-  x3d::vector2 dA = RotAT * dp;
-  x3d::vector2 dB = RotBT * dp;
-
-  x3d::matrix2x2 C = RotAT * RotB;
-  x3d::matrix2x2 absC = C.abs();
-  x3d::matrix2x2 absCT = absC.transpose();
-
-  // Box A faces
-  x3d::vector2 faceA = dA.abs() - hA - absC * hB;
-  x3d::vector2 faceB = dB.abs() - absCT * hA - hB;
-  if (faceA.u0() > 0.0f || faceA.u1() > 0.0f || faceB.u0() > 0.0f
-    || faceB.u1() > 0.0f) {
+  if (np2 < 2) {
     return 0;
   }
-
-  // Find best axis
-  float separation[4];
-  separation[0] = -faceA.u0();
-  separation[1] = -faceA.u1();
-  separation[2] = -faceB.u0();
-  separation[3] = -faceB.u1();
-
-  size_t idx = idxOfMin(separation);
-
-  Axis axis = (Axis)idx;
-  x3d::vector2 normal;
-
-  // Setup clipping plane data based on the separating axis
-  x3d::vector2 frontNormal, sideNormal;
-  ClipVertex incidentEdge[2];
-  float front, negSide, posSide;
-  char negEdge, posEdge;
-
-  // Compute the clipping lines and the line segment to be clipped.
-  switch (axis) {
-  case FACE_A_X: {
-    normal = dA.u0() > 0.0f ? RotA.col(0) : -RotA.col(0);
-    frontNormal = normal;
-    front = posA * frontNormal + hA.u0();
-    sideNormal = RotA.col(1);
-    float side = posA * sideNormal;
-    negSide = -side + hA.u1();
-    posSide = side + hA.u1();
-    negEdge = EDGE3;
-    posEdge = EDGE1;
-    ComputeIncidentEdge(incidentEdge, hB, posB, RotB, frontNormal);
-  } break;
-
-  case FACE_A_Y: {
-    normal = dA.u1() > 0.0f ? RotA.col(1) : -RotA.col(1);
-    frontNormal = normal;
-    front = posA * frontNormal + hA.u1();
-    sideNormal = RotA.col(0);
-    float side = posA * sideNormal;
-    negSide = -side + hA.u0();
-    posSide = side + hA.u0();
-    negEdge = EDGE2;
-    posEdge = EDGE4;
-    ComputeIncidentEdge(incidentEdge, hB, posB, RotB, frontNormal);
-  } break;
-
-  case FACE_B_X: {
-    normal = dB.u0() > 0.0f ? RotB.col(0) : -RotB.col(0);
-    frontNormal = -normal;
-    front = posB * frontNormal + hB.u0();
-    sideNormal = RotB.col(1);
-    float side = posB * sideNormal;
-    negSide = -side + hB.u1();
-    posSide = side + hB.u1();
-    negEdge = EDGE3;
-    posEdge = EDGE1;
-    ComputeIncidentEdge(incidentEdge, hA, posA, RotA, frontNormal);
-  } break;
-
-  case FACE_B_Y: {
-    normal = dB.u1() > 0.0f ? RotB.col(1) : -RotB.col(1);
-    frontNormal = -normal;
-    front = posB * frontNormal + hB.u1();
-    sideNormal = RotB.col(0);
-    float side = posB * sideNormal;
-    negSide = -side + hB.u0();
-    posSide = side + hB.u0();
-    negEdge = EDGE2;
-    posEdge = EDGE4;
-    ComputeIncidentEdge(incidentEdge, hA, posA, RotA, frontNormal);
-  } break;
-  }
-
-  // clip other face with 5 box planes (1 face plane, 4 edge planes)
-
-  ClipVertex clipPoints1[2];
-  ClipVertex clipPoints2[2];
-  int np;
-
-  // Clip to box side 1
-  np = ClipSegmentToLine(clipPoints1, incidentEdge, -sideNormal, negSide, negEdge);
-
-  if (np < 2)
-    return 0;
-
-  // Clip to negative box side 1
-  np = ClipSegmentToLine(clipPoints2, clipPoints1, sideNormal, posSide, posEdge);
-
-  if (np < 2)
-    return 0;
 
   // Now clipPoints2 contains the clipping points.
   // Due to roundoff, it is possible that clipping removes all points.
-  //ClipVertex* cv = clipPoints2;
   ClipVertex* cv = incedge2;
 
   int numContacts = 0;
   for (int i = 0; i < 2; ++i) {
-    float separation = frontNormal * cv[i].v - front;
+    float separation = xnormal * cv[i].v - frontOffset;
 
     if (separation <= 0) {
       contacts[numContacts].separation = separation;
-      contacts[numContacts].normal = normal;
+      contacts[numContacts].normal = edge->flip ? -xnormal : xnormal;
       // slide contact point onto reference face (easy to cull)
-      contacts[numContacts].position = cv[i].v - separation * frontNormal;
+      contacts[numContacts].position = cv[i].v - separation * xnormal;
       contacts[numContacts].feature = cv[i].fp;
-      if (axis == FACE_B_X || axis == FACE_B_Y)
+      if (edge->flip) {
         Flip(contacts[numContacts].feature);
+      }
       ++numContacts;
     }
   }
